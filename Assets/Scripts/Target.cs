93cfded349cc4a92b8e9e62d157793a35e7031f6
Maxie Bernt <maxie.bernt@hotmail.de>
2025-11-09 12:37:12 +0100

    ADDED player tag to shrooms, target prefab, game end condition; ADAPTED ingame ui

---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Scripts
+{
+    public class Target : MonoBehaviour
+    {
+        [SerializeField] private MyGameManager gameManager;
+        void OnTriggerEnter(Collider collider)
+        {
+            if (collider.tag == "Player")
+            {
+                gameManager.ChangeGameState(GameState.GameOver);
+            }
+        }
+    }
+}
